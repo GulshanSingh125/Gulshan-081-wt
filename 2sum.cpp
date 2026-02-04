18cf8488c9819f6abdab4de660b4ad53ee5edf28
GulshanSingh125 <gulshansingh.1252005.gs@gmail.com>
2026-02-04 22:17:16 +0530

    added new commit for 2sum

--- 2sum.cpp
+++ 2sum.cpp
@@ -1,9 +1,39 @@
-#include<stdio.h>
-#include<iostream>
-int arr[] = {1,2,3,4}
-int n
-void main()
-{
-	out<<"2-sum Algo"<<endl;
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool twoSum(vector<int> &arr, int target) {
+    int n = arr.size();
+
+    for (int i = 0; i < n; i++) {
+      
+        // For each element arr[i], check every
+        // other element arr[j] that comes after it
+        for (int j = i + 1; j < n; j++) {
+          
+            // Check if the sum of the current pair
+            // equals the target
+            if (arr[i] + arr[j] == target) {
+                return true;
+            }
+        }
+    }
+  
+    // If no pair is found after checking
+    // all possibilities
+    return false;
+}
+
+int main() {
+  
+    vector<int> arr = {0, -1, 2, -3, 1};
+    int target = -2;
+
+    if(twoSum(arr, target))
+      cout << "true";
+    else
+      cout << "false";
+
+    return 0;
 }
 
